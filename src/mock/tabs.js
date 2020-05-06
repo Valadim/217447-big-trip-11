b5b626d303f25abd0fd1af2ef25690caf37d289b
Vadim <sakeyev@mail.ru>
2020-05-06 17:58:07 +0600

    rewrote classes on componentsЭ

---
+++ src/mock/tabs.js
@@ -0,0 +1,11 @@
+export const menuItems = [`Table`, `Stats`, `Join`];
+
+// const generateMenuItems = () => {
+//   return menuNames.map((it) => {
+//     return {
+//       name: it,
+//     };
+//   });
+// };
+//
+// export {generateMenuItems};
