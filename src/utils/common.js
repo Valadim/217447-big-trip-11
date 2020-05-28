c2744d6f5d808b1d5981e3777053567b9b0a80db
Vadim <sakeyev@mail.ru>
2020-05-28 13:38:15 +0600

    Module7-task1 WIP

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,18 +1,77 @@
-const castTimeFormat = (value) => {
-  return value < 10 ? `0${value}` : String(value);
+import moment from "moment";
+
+export const getRandomIntegerNumber = (min, max) => {
+  return min + Math.floor(Math.random() * (max - min));
 };
 
-export const formatTime = (date) => {
-  const hours = castTimeFormat(date.getHours() % 12);
-  const minutes = castTimeFormat(date.getMinutes());
-
-  return `${hours}:${minutes}`;
+export const getRandomArrayItems = (arr) => {
+  const randomIndex = getRandomIntegerNumber(0, arr.length);
+  return arr[randomIndex];
 };
 
-export const formatDate = (date) => {
-  const day = castTimeFormat(date.getDate());
-  const month = castTimeFormat(date.getMonth() + 1);
-  const year = date.getFullYear() % 100;
+export const shuffleArray = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
 
-  return day + `/` + month + `/` + year;
+export const capitalizeFirstLetter = ([initial, ...rest]) => [initial.toUpperCase(), ...rest].join(``);
+
+export const parseTime = (date) => {
+  return moment(date).format(`HH:mm`);
+};
+
+export const parseDate = (date) => {
+  return moment(date).format(`DD MMM`);
+};
+
+const getWaypointsCost = (arr) => arr.reduce((acc, it) => parseInt(acc, 10) + parseInt(it.price, 10), 0);
+
+const getOffersCost = (arr) => {
+  let sum = 0;
+  arr.forEach((offer) => {
+    sum += offer.offers.reduce((acc, it) => parseInt(acc, 10) + parseInt(it.price, 10), 0);
+  });
+  return sum;
+};
+
+export const getFullPrice = (arr) => {
+  const totalCostElement = document.querySelector(`.trip-info__cost-value`);
+  totalCostElement.textContent = getWaypointsCost(arr) + getOffersCost(arr);
+};
+
+export const disableComponent = (className) => {
+  const component = document.querySelector(`.${className}`);
+  if (!component.getAttribute(`disabled`)) {
+    component.setAttribute(`disabled`, `disabled`);
+  }
+};
+
+export const enableComponent = (className) => {
+  const component = document.querySelector(`.${className}`);
+  if (component.getAttribute(`disabled`)) {
+    component.removeAttribute(`disabled`);
+  }
+};
+
+export const createOffersSet = (data) => {
+  return data.reduce((acc, item) => {
+    if (acc[item.type] === undefined) {
+      acc[item.type] = [];
+    }
+    acc[item.type] = item.offers;
+    return acc;
+  }, {});
+};
+
+export const createDestinationsSet = (data) => {
+  return data.reduce((acc, item) => {
+    if (acc[item.name] === undefined) {
+      acc[item.name] = [];
+    }
+    acc[item.name] = item;
+    return acc;
+  }, {});
 };
